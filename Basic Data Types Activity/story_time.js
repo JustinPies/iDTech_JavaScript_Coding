fab9c478d69c074694ce61dc8d133609cf5d53c4
Justin <justin.pierce.06.25@gmail.com>
2022-07-20 10:21:59 -0500

    Update

---
+++ Basic Data Types Activity/story_time.js
@@ -0,0 +1,32 @@
+// TODO: Create 4-String variables to introduce develop your story.
+// YOUR CODE GOES HERE
+let name = "Justin"
+let greeting = "Hello there!"
+let title = "Dragon Slayer"
+let islandName = "Adventure Island"
+
+// TODO: Create 3-String variables to set the time period of your story or discuss other number elements.
+// YOUR CODE GOES HERE
+let age = 17
+let year = 2022
+let century = 21
+
+// TODO: Create 1-Array variable to show a collection of items your character might have.
+// YOUR CODE GOES HERE
+let items = ["Backpack", "Sword", "Sheild", "Bananas", "Potions"]
+
+// TODO: Create 1-Boolean variable to demonstrate a true or false scenario.
+// YOUR CODE GOES HERE
+let hasWeapon = true
+
+// TODO: Print your story to the console.
+// YOUR CODE GOES HERE
+console.log(greeting + " My name is " + name + "!")
+console.log("I am the " + title + ".")
+console.log("This islands name is " + islandName + ".")
+console.log("I am currently " + age + " years old.")
+console.log("The year is " + year + " and it is the " + century + "st century.")
+console.log("My favorite food is " + items[3] + ".")
+console.log("It is " + hasWeapon + " that I have weapons.")
+console.log("The tools I use for combat are a " + items[1] + " and " + items[2] + ".")
+console.log("Inside of my " + items[0] + " I have some " + items[4] + ".")
